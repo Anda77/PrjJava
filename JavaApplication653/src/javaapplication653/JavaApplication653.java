05cc79838814cdfffb26d4464aa365ae174d0590
Anda77 <andadeacu@yahoo.com>
2017-08-02 16:28:31 +0300

    Commit sample I/O

---
+++ JavaApplication653/src/javaapplication653/JavaApplication653.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication653;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+     
+    
+    
+
+public class JavaApplication653 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static Map<Integer,Print> map = new HashMap<>();
+     
+    static{
+         
+        DataInputStream i = null;
+        boolean eof = false;
+     
+        try{
+     
+          i = new DataInputStream(new BufferedInputStream(new FileInputStream("file.dat")));
+             
+          while(!eof){
+           
+          int data1 = i.readInt();
+          String data2 = i.readUTF();
+           
+          map.put(data1, new Print(data1,data2));
+             
+          }
+             
+        }catch(IOException e){
+             
+            eof = true;
+            try{
+            i.close();
+            }catch(IOException p){
+                     
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+       
+    }
+
+}
---
+++ JavaApplication653/src/javaapplication653/Print.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication653;
+
+
+import java.util.*;
+import java.io.*;
+ 
+ 
+public class Print {
+ 
+    private final int num;
+    private final String name;
+    public Print(int num, String name) {
+         
+        this.num = num;
+        this.name = name;
+    }
+     
+     
+    public int getNum() {
+        return num;
+    }
+    public String getName() {
+        return name;
+    }
+     
+     
+     
+}
