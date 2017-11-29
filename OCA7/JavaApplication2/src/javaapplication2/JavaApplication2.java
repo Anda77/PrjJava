508627acf0a033f345ea91eba938085744e11796
Anda77 <andadeacu@yahoo.com>
2017-11-29 20:22:17 +0200

    Update JavaApplication2.java

--- OCA7/JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ OCA7/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -5,15 +5,8 @@
  */
 package javaapplication2;
 
-/**
- *
- * @author anda
- */
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         int a[] = {1, 2, 053, 4};
